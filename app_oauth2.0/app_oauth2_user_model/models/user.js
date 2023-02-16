a660aeccdd0614c91dc00ebb0aa1ddc8840712b2
mozdalif <mozsocia@gmail.com>
2023-02-17 01:24:32 +0600

    Update user.js

--- app_oauth2.0/app_oauth2_user_model/models/user.js
+++ app_oauth2.0/app_oauth2_user_model/models/user.js
@@ -3,6 +3,7 @@ const { Schema } = mongoose;
 
 const userSchema = new Schema({
   email: { type: String, required: true },
+  name: { type: String, required: true },
   password: { type: String }
 });
 
