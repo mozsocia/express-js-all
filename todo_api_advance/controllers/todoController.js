556f66a48476d78485df88b08676d0e1424e3e3d
mozdalif <mozsocia@gmail.com>
2023-04-28 20:11:33 +0600

    http status code updated

--- todo_api_advance/controllers/todoController.js
+++ todo_api_advance/controllers/todoController.js
@@ -1,41 +1,31 @@
 const Todo = require('../models/todo');
 const { validationResult, body } = require('express-validator');
+const asyncHandler = require('express-async-handler');
+const { StatusCodes } = require('http-status-codes');
 
-exports.getAllTodos = async (req, res) => {
+exports.getAllTodos = asyncHandler(async (req, res) => {
+  const todos = await Todo.find({});
+  res.status(StatusCodes.OK).json(todos);
+});
 
-
-
-  try {
-    const todos = await Todo.find({});
-    res.status(200).json(todos);
-  } catch (error) {
-    res.status(500).json({ error: error.message });
+exports.getTodoById = asyncHandler(async (req, res) => {
+  const { id } = req.params;
+  const todo = await Todo.findById(id);
+  if (!todo) {
+    return res.status(NOT_FOUND).json({ error: 'Todo not found' });
   }
-};
+  res.status(StatusCodes.OK).json(todo);
+});
 
-exports.getTodoById = async (req, res) => {
-  try {
-    const { id } = req.params;
-    const todo = await Todo.findById(id);
-    if (!todo) {
-      return res.status(404).json({ error: 'Todo not found' });
-    }
-    res.status(200).json(todo);
-  } catch (error) {
-    res.status(500).json({ error: error.message });
-  }
-};
-
-exports.createTodo = async (req, res) => {
-
-
+exports.createTodo = asyncHandler(async (req, res) => {
   const validationRules = [
-
-    body('title')
-      .notEmpty().withMessage('title is required'),
+    body('title').notEmpty().withMessage('title is required'),
     body('description')
-      .notEmpty().withMessage('description is required').bail()
-      .isLength({ min: 20 }).withMessage('description must be at least 20 characters')
+      .notEmpty()
+      .withMessage('description is required')
+      .bail()
+      .isLength({ min: 20 })
+      .withMessage('description must be at least 20 characters'),
   ];
 
   // Run validation
@@ -44,53 +34,45 @@ exports.createTodo = async (req, res) => {
   // Check for validation errors
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
-    return res.status(422).json({ errors: errors.array() });
+    return res.status(StatusCodes.UNPROCESSABLE_ENTITY).json({ errors: errors.array() });
   }
 
+  const { title, description } = req.body;
+  const todo = new Todo({
+    title,
+    description,
+  });
 
+  const newTodo = await todo.save();
+  res.status(StatusCodes.CREATED).json(newTodo);
+});
 
-  try {
-    const { title, description } = req.body;
-    const todo = new Todo({
-      title,
-      description
-    });
-    const newTodo = await todo.save();
-    res.status(201).json(newTodo);
-  } catch (error) {
-    res.status(500).json({ error: error.message });
+exports.updateTodo = asyncHandler(async (req, res) => {
+  const { id } = req.params;
+  const { title, description, completed } = req.body;
+
+  const todo = await Todo.findByIdAndUpdate(
+    id,
+    { title, description, completed },
+    { new: true }
+  );
+
+  if (!todo) {
+    return res.status(StatusCodes.NOT_FOUND).json({ error: 'Todo not found' });
   }
 
-  res.json({ "msg": "success" })
-};
+  res.status(StatusCodes.OK).json(todo);
+});
 
-exports.updateTodo = async (req, res) => {
-  try {
-    const { id } = req.params;
-    const { title, description, completed } = req.body;
-    const todo = await Todo.findByIdAndUpdate(
-      id,
-      { title, description, completed },
-      { new: true }
-    );
-    if (!todo) {
-      return res.status(404).json({ error: 'Todo not found' });
-    }
-    res.status(200).json(todo);
-  } catch (error) {
-    res.status(500).json({ error: error.message });
-  }
-};
+exports.deleteTodo = asyncHandler(async (req, res) => {
+  const { id } = req.params;
+  const todo = await Todo.findByIdAndDelete(id);
 
-exports.deleteTodo = async (req, res) => {
-  try {
-    const { id } = req.params;
-    const todo = await Todo.findByIdAndDelete(id);
-    if (!todo) {
-      return res.status(404).json({ error: 'Todo not found' });
-    }
-    res.status(204).send();
-  } catch (error) {
-    res.status(500).json({ error: error.message });
+  if (!todo) {
+    return res.status(StatusCodes.NOT_FOUND).json({ error: 'Todo not found' });
   }
-};
+
+  res.status(StatusCodes.NO_CONTENT).send();
+});
+
+
